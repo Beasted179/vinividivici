da798533e4865b5274396c644ff3e79b9c132d07
Beasted179 <clarios179@gmail.com>
2023-08-15 12:35:46 -0700

    changed api endpoint links

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 // api.js
-const baseUrl = 'https://vinifrontend.onrender.com';
+const baseUrl = 'https://vinividivici.onrender.com';
 export async function sendKeytoBackend(apiKey) {
 
   console.log('Sending API key:', apiKey);
 
   try {
-    const response = await fetch(`https://vinifrontend.onrender.com/api/authenticate`, {
+    const response = await fetch(`${baseUrl}/api/authenticate`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -31,7 +31,7 @@ export async function sendKeytoBackend(apiKey) {
 
 export async function fetchUser(token) {
   try {
-    const response = await fetch(`https://vinifrontend.onrender.com/api/user`, {
+    const response = await fetch(`${baseUrl}/api/user`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${token}`, // Include the JWT token in the Authorization header
@@ -55,7 +55,7 @@ export async function fetchUser(token) {
 
 export async function fetchTables(token) {
   try {
-    const response = await fetch(`https://vinifrontend.onrender.com/api/tables`, {
+    const response = await fetch(`${baseUrl}/api/tables`, {
       headers: {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',
